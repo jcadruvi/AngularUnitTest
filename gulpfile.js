7bf382f2688d433da64e6988a97d5f89e0a54ede
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-07-28 21:01:38 -0700

    Adding the reload tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -10,40 +10,72 @@ var config = {
     templatesFolder: 'templates/**/*.html'
 };
 
+var doSassTask = function () {
+    return gulp.src(config.sassFolder)
+               .pipe(sass().on('error', sass.logError))
+               .pipe(gulp.dest('./dist/content/css'));
+};
+
+var doScriptsTask = function () {
+    return gulp.src(config.jsFolder)
+                .pipe(gulp.dest('./dist/scripts'));
+};
+
+var doTemplatesTask = function () {
+    return gulp.src(config.templatesFolder)
+                .pipe(ngTemplates({
+                    filename: 'templates.js',
+                    moduleName: 'app',
+                    path: function (path, base) {
+                        return path.replace(base, '').replace('/templates', '');
+                    }
+                }))
+                .pipe(gulp.dest('dist/scripts'));
+};
+
+var doWatchTask = function () {
+    gulp.watch(config.jsFolder, ['scripts']);
+    gulp.watch(config.sassFolder, ['sass']);
+    gulp.watch(config.templatesFolder, ['templates']);
+};
+
 gulp.task('bower', function() {
-  return bower()
-    .pipe(gulp.dest('dist/bower'))
+    return bower()
+             .pipe(gulp.dest('dist/bower'))
 });
 
-gulp.task('sass', function () {
-  gulp.src(config.sassFolder)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/content/css'))
-      .pipe(livereload());
+gulp.task('sass', function (){
+    doSassTask();
 });
 
-gulp.task('watch', function () {
-  livereload.listen();
-  gulp.watch(config.jsFolder, ['scripts']);
-  gulp.watch(config.sassFolder, ['sass']);
-  gulp.watch(config.templatesFolder, ['templates']);
+gulp.task('sass:reload', function () {
+    doSassTask().pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    doWatchTask();
+});
+
+gulp.task('watch:reload', function () {
+    livereload.listen();
+    doWatchTask();
 });
 
 gulp.task('scripts', function () {
-  gulp.src(config.jsFolder)
-    .pipe(gulp.dest('./dist/scripts'));
+    doScriptsTask();
+});
+
+gulp.task('scripts:reload', function () {
+    doScriptsTask().pipe(livereload());
 });
 
 gulp.task('templates', function () {
-    gulp.src(config.templatesFolder)
-        .pipe(ngTemplates({
-            filename: 'templates.js',
-            moduleName: 'app',
-            path: function (path, base) {
-                return path.replace(base, '').replace('/templates', '');
-            }
-        }))
-        .pipe(gulp.dest('dist/scripts'));
+    doTemplatesTask();
+});
+
+gulp.task('templates:reload', function () {
+    doTemplatesTask().pipe(livereload());
 });
 
 gulp.task('dev', ['bower', 'sass', 'scripts', 'templates', 'watch']);
+gulp.task('dev:reload', ['bower', 'sass:reload', 'scripts:reload', 'templates:reload', 'watch:reload']);
