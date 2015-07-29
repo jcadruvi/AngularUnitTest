3ac9bc86e02adb5a7d630ebe2d2d2249b673b28f
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-07-28 20:27:41 -0700

    Adding the watch task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var bower = require('gulp-bower');
 var ngTemplates = require('gulp-ng-template');
 var sass = require('gulp-sass');
 
-var sassFolder = './content/**/*.scss';
+var config = {
+    jsFolder: 'scripts/**/*.js',
+    sassFolder: './content/**/*.scss',
+    templatesFolder: 'templates/**/*.html'
+};
 
 gulp.task('bower', function() {
   return bower()
@@ -11,22 +15,24 @@ gulp.task('bower', function() {
 });
 
 gulp.task('sass', function () {
-  gulp.src(sassFolder)
+  gulp.src(config.sassFolder)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/content/css'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch(sassFolder, ['sass']);
+gulp.task('watch', function () {
+  gulp.watch(config.jsFolder, ['scripts']);
+  gulp.watch(config.sassFolder, ['sass']);
+  gulp.watch(config.templatesFolder, ['templates']);
 });
 
 gulp.task('scripts', function () {
-  gulp.src('scripts/**/*.js')
+  gulp.src(config.jsFolder)
     .pipe(gulp.dest('./dist/scripts'));
 });
 
 gulp.task('templates', function () {
-    gulp.src('templates/**/*.html')
+    gulp.src(config.templatesFolder)
         .pipe(ngTemplates({
             filename: 'templates.js',
             moduleName: 'app',
@@ -37,4 +43,4 @@ gulp.task('templates', function () {
         .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('dev', ['bower', 'sass', 'scripts', 'templates']);
+gulp.task('dev', ['bower', 'sass', 'scripts', 'templates', 'watch']);
