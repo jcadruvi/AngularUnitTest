e3cdf72fba253bc97f65abc051965f2e183a3afd
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-10-04 15:56:19 -0700

    Simple directive unit test is working.

--- karma.config.js
+++ karma.config.js
@@ -5,7 +5,10 @@ module.exports = function(config) {
     files: [
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'scripts/**/*.js',
+      'dist/scripts/templates.js',
       'tests/**/*.spec.js'
     ],
     angularFilesort: {
--- tests/jcDropdown.spec.js
+++ tests/jcDropdown.spec.js
@@ -1,6 +1,20 @@
 describe('jcDropdown', function (){
-   it('testing', function () {
-        var test = true;
-        expect(test).toEqual(true);
-   });
+    var $compile,
+        $rootScope;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_){
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('testing', function () {
+        var $scope,
+            element;
+        $scope = $rootScope.$new();
+        element = $compile('<jc-dropdown></jc-dropdown>')($scope);
+        $scope.$digest();
+        expect(element.html()).toEqual('This is the dropdown directive.');
+    });
 });
