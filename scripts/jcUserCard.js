592f7302e6ababce9529e6e59af015c1e70e14f5
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-11-09 07:31:58 -0800

    Adding the jcUserCard directive and template.

---
+++ scripts/jcUserCard.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular.module('app').directive('jcUserCard', jcUserCard);
+
+    function jcUserCard() {
+        return {
+            replace: true,
+            restrict: 'E',
+            scope: {
+            },
+            templateUrl: 'jcUserCard.html'
+        };
+	}
+})();
+
