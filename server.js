daa41a512184a1ad33cebfc069d40c12318bff9e
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-11-14 14:33:53 -0800

    Adding node server.js file and a test index page.

---
+++ server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//var controllers = require('./app/controllers');
+var express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    path = require('path');
+
+
+// Load configurations
+// Set the node environment variable if not set before
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+//var config = require('./config/config');
+
+var port = 8080;
+
+app.set('port', port);
+app.set('views', path.join(__dirname, 'app/views'));
+
+app.set('view engine', 'jade');
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(express.cookieParser('your secret here'));
+//app.use(express.session());
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public/dist')));
+
+fs.readdirSync('./app/controllers').forEach(function (file) {
+    var route;
+    if(file.substr(-3) === '.js') {
+        route = require('./app/controllers/' + file);
+        route.controller(app);
+    }
+});
+
+// Start the app by listening on <port>
+app.listen(port, function(){
+    console.log('Express app started on port ' + port);
+});
+
+
