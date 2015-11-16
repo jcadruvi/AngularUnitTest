83c11a8b429b6341da2bb2e114c937dede4e7997
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-11-16 07:10:36 -0800

    Adding the get multiple users api.

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -13,4 +13,56 @@ module.exports.controller = function (app) {
             }
         });
     });
+
+    app.get('/api/users', function(req, res) {
+        res.json({
+            "data": [
+                 {
+                     city: "San Jose",
+                     email: "johndoe@gmail.com",
+                     name: "John Doe",
+                     phone: "(123) 456-7890",
+                     state: "CA"
+                },
+                {
+                     city: "San Fransisco",
+                     email: "janedoe@gmail.com",
+                     name: "Jane Doe",
+                     phone: "(123) 456-7891",
+                     state: "CA"
+                },
+                {
+                     city: "San Jose",
+                     email: "johnsmith@gmail.com",
+                     name: "John Smith",
+                     phone: "(123) 456-7892",
+                     state: "CA"
+                },
+                {
+                     city: "San Diego",
+                     email: "janesmith@gmail.com",
+                     name: "Jane Smith",
+                     phone: "(123) 456-7893",
+                     state: "CA"
+                },
+                {
+                     city: "San Jose",
+                     email: "fredsmith@gmail.com",
+                     name: "Fred Smith",
+                     phone: "(123) 456-7894",
+                     state: "CA"
+                },
+                {
+                     city: "San Fransisco",
+                     email: "joshsmith@gmail.com",
+                     name: "Josh Smith",
+                     phone: "(123) 456-7895",
+                     state: "CA"
+                }
+            ],
+            "status": {
+                "code": 200
+            }
+        });
+    });
 };
