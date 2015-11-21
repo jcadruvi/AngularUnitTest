310497d5924784903eb1222b696dc3828390e142
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-11-21 14:42:10 -0800

    Adding the users to the index controller.

--- public/src/scripts/indexController.js
+++ public/src/scripts/indexController.js
@@ -1,21 +1,14 @@
-angular.module('app').controller('indexController', ['$scope', indexController]);
+angular.module('app').controller('indexController', ['$scope', 'userService', indexController]);
 
-function indexController($scope) {
+function indexController($scope, userService) {
     $scope.items = [
         { id: 1, description: "item 1"},
         { id: 2, description: "item 2"},
         { id: 3, description: "item 3", selected: true }
     ];
 
-    $scope.users = [
-        {
-            city: "San Jose",
-            email: "jdoe@gmail.com",
-            name: "John Doe",
-            phone: "(123) 456-7890",
-            state: "CA"
-        }
-    ];
+    $scope.service = userService;
+    userService.init();
 
     $scope.onDropdownSelect = function(item) {
         console.log(item);
