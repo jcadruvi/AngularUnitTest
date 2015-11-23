7f3c6a40c9bd7852573990fa552735cb3278ccd5
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-11-22 21:18:00 -0800

    Using a different pattern for controller as.

--- public/src/scripts/indexController.js
+++ public/src/scripts/indexController.js
@@ -1,16 +1,18 @@
 angular.module('app').controller('indexController', ['$scope', 'userService', indexController]);
 
 function indexController($scope, userService) {
-    this.items = [
+    var vm = this;
+
+    vm.items = [
         { id: 1, description: "item 1"},
         { id: 2, description: "item 2"},
         { id: 3, description: "item 3", selected: true }
     ];
 
-    this.service = userService;
+    vm.service = userService;
     userService.init();
 
-    this.onDropdownSelect = function(item) {
+    vm.onDropdownSelect = function(item) {
         console.log(item);
     };
 }
