b37dd4adc15302d294e153172bc121831f471707
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-12-09 17:45:57 -0800

    Stashing the changes I made to unit tests.

--- tests/public/src/scripts/jcUserCard.spec.js
+++ tests/public/src/scripts/jcUserCard.spec.js
@@ -1,3 +1,36 @@
 describe('jcUserCard', function() {
 
+    var $compile,
+        $rootScope;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function(_$compile_, _$rootScope_){
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('should display user name.', function() {
+        var $element,
+            $scope,
+            element,
+            name,
+            user;
+
+        user = {
+            city: "San Jose",
+            email: "johndoe@gmail.com",
+            name: "John Doe",
+            phone: "(123) 456-7890",
+            state: "CA"
+        };
+        $scope = $rootScope.$new();
+        $scope.name = user.name;
+        $scope.email = user.email;
+        element = angular.element('<jc-user-card name="name" email="email" phone="phone" city="city" state="state"></jc-user-card>');
+        $compile(element, $scope);
+        $scope.$digest();
+        $element = $(element);
+        //expect($element.find('.user-name strong').html()).toBe("John Doe");
+    });
 });
