2b343c310483c6d714761577839dfd5102061bf6
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-12-11 17:35:58 -0800

    Adding the jcGridController.

---
+++ public/src/scripts/jcGridController.js
@@ -0,0 +1,11 @@
+(function(){
+    'use strict';
+    angular.module('app')
+        .controller('jcGridController', ['$scope', jcGridController]);
+
+    function jcGridController($scope) {
+        var vm = this;
+
+
+    }
+})();
