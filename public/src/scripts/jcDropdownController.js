ed6df3970f7c870ae461c8a4461f69f55dba28c8
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-12-15 07:45:18 -0800

    Adding strict to controller definition.

--- public/src/scripts/jcDropdownController.js
+++ public/src/scripts/jcDropdownController.js
@@ -1,27 +1,30 @@
-angular.module('app').controller('jcDropdownController', ['$scope', jcDropdownController]);
+(function(){
+    'use strict';
+    angular.module('app').controller('jcDropdownController', ['$scope', jcDropdownController]);
 
-function jcDropdownController($scope) {
-    var vm = this;
-    vm.hasFocus = false;
-    vm.showDialog = false;
-    vm.text = '';
-    $scope.$watch('items', function(){
-       for(var i=0; vm.items && i < vm.items.length; i++) {
-           if(vm.items[i].selected) {
-               vm.text = vm.items[i].description;
-           }
-       }
-    });
-    vm.onItemClick = function(item) {
-        vm.showDialog = false;
+    function jcDropdownController($scope) {
+        var vm = this; // jshint ignore:line
         vm.hasFocus = false;
-        vm.text = item.description;
-        for (var i=0; vm.items && i < vm.items.length; i++) {
-            vm.items[i].selected = false;
-        }
-        item.selected = true;
-        if(vm.onSelect) {
-            vm.onSelect(item);
-        }
-    };
-}
+        vm.showDialog = false;
+        vm.text = '';
+        $scope.$watch('items', function(){
+           for(var i=0; vm.items && i < vm.items.length; i++) {
+               if(vm.items[i].selected) {
+                   vm.text = vm.items[i].description;
+               }
+           }
+        });
+        vm.onItemClick = function(item) {
+            vm.showDialog = false;
+            vm.hasFocus = false;
+            vm.text = item.description;
+            for (var i=0; vm.items && i < vm.items.length; i++) {
+                vm.items[i].selected = false;
+            }
+            item.selected = true;
+            if(vm.onSelect) {
+                vm.onSelect(item);
+            }
+        };
+    }
+})();
